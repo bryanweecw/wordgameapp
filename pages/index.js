f8a2342e8f64bb9ea5b7244d22eb607acb4b9733
Bryan <bryan.wee@u.yale-nus.edu.sg>
2022-01-24 17:05:01 +0800

    newcommit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,50 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import MyApp from './_app'
+import data from './test1.json';
+import React, { useEffect, useState, useRef } from "react";
+import Result from './result';
 
-export default function Home() {
-  
-  return (
+function MyApp() {
+  const [secretWord, setSecretWord] = useState([]);
+  const [guess, setGuess] = useState([])
+  const [turn, setTurn] = useState(0)
+  useEffect(() => {
+    setSecretWord((data[Math.floor(Math.random() * data.length)]))
+  }, []);
+
+  const formOneSubmit = (e) => {
+    e.preventDefault();
+    let elements = document.getElementsByName("userguess");
+    let workingArr = []
+    for(let i = 0; i < elements.length; i++){
+      workingArr.push(elements[i].value)
+    }
+    console.log(workingArr)
+    setGuess(workingArr)
+    console.log(guess)
+    document.form1.reset();
+    setTurn(turn+1)
+  }
+
+
+  return(
     <>
-    <MyApp />
+    {secretWord}
+    <form onSubmit = {formOneSubmit} name="form1">
+      <input name="userguess" type="text" maxLength="1" className='block' required/>
+      <input name="userguess" type="text" maxLength="1" className='block' required/>
+      <input name="userguess" type="text" maxLength="1" className='block' required/>
+      <input name="userguess" type="text" maxLength="1" className='block' required/>
+      <input name="userguess" type="text" maxLength="1" className='block' required/>
+      <button type="submit">guess</button>
+    </form>
+    <button onClick={(e) => {
+      setSecretWord(data[Math.floor(Math.random() * data.length)]);
+      document.form1.reset();
+      setTurn(0);
+      setGuess([]);
+      }}>new word</button>
+    <div><Result answer={secretWord} guess={guess}/>{turn}{guess.length}</div>
     </>
   )
 }
+
+export default MyApp
